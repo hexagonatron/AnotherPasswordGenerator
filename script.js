c9cef7ec0d3722365991fd158bf7486944849f27
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-03-07 15:12:29 +1030

    Adding fns in JS to run necessary tasks

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Selecting inputs and outputs from DOM
+
 let lengthSlider = document.querySelector("#password-length-slider");
 
 let lengthText = document.querySelector("#password-length-box");
@@ -10,6 +12,40 @@ let numberSwitch = document.querySelector(".number-switch");
 
 let symbolSwitch = document.querySelector(".symbol-switch");
 
+let genPasswordBut = document.querySelector("#gen-button");
+
+let copyBut = document.querySelector("#copy-button");
+
+let genPopupBut = document.querySelector("#gen-popup-button");
+
+//Functions to generate specific random characters
+
+
+
+
+genPopups = () => {
+
+}
+
+genPassword = () => {
+
+}
+
+copyPassword = () => {
+    
+}
+
+
+
+//Attaching event listeners to buttons
+genPopupBut.addEventListener("click", genPopups());
+
+genPasswordBut.addEventListener("click", genPassword());
+
+copyBut.addEventListener("click", copyPassword());
+
+
+//Event listeners to detect change in password length slider or change in password length text box
 lengthSlider.addEventListener("input", (e) => {
     console.log(lengthSlider.value);
     lengthText.value = lengthSlider.value;
@@ -18,4 +54,4 @@ lengthSlider.addEventListener("input", (e) => {
 lengthText.addEventListener("input", (e) => {
     console.log(lengthText.value);
     lengthSlider.value = lengthText.value;
-})
+});
